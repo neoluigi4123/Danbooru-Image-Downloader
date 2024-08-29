6e1135dd49ebd26b0946258b0de5e6ca458076de
néo Luigi <85024393+neoluigi4123@users.noreply.github.com>
2024-08-29 08:41:44 +0200

    New update 1.1
    This new update (1.1):
     - the .bat now install the python script
     - the app detect when its outdated, showing a new button redirecting on the main page

--- install.bat
+++ install.bat
@@ -5,7 +5,6 @@ python --version | findstr /r "3\.10"
 if %errorlevel% neq 0 (
     echo Python 3.10 is not installed.
     echo Downloading and installing Python 3.10...
-    :: Replace the URL with the actual download URL for Python 3.10
     curl -o python-3.10.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
     start /wait python-3.10.exe /quiet InstallAllUsers=1 PrependPath=1
     del python-3.10.exe
@@ -35,8 +34,21 @@ if %errorlevel% neq 0 (
     python -m pip install pygelbooru
 )
 
+:: Download the "latest" file to determine the latest version of the script
+echo Downloading the latest version information...
+curl -L -o latest_version.txt https://raw.githubusercontent.com/neoluigi4123/Gelbooru-Image-Downloader/main/latest
+
+:: Read the latest version number from the file
+set /p VERSION=<latest_version.txt
+echo Latest version is %VERSION%
+
+:: Construct the download URL based on the version number
+set DOWNLOAD_URL=https://raw.githubusercontent.com/neoluigi4123/Gelbooru-Image-Downloader/main/gelbooru_image_downloader_%VERSION%.py
+echo Downloading %DOWNLOAD_URL%...
+curl -L -o gelbooru_image_downloader.py %DOWNLOAD_URL%
+
 :: Launch the app
-echo Launching gelbooru_image_downloader_1.0.py...
-python gelbooru_image_downloader_1.0.py
+echo Launching gelbooru_image_downloader.py...
+python gelbooru_image_downloader.py
 
 pause
